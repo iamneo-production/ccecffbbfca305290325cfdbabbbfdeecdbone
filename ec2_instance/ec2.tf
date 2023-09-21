0517906d7c195c976017d36327f7b1aae8bce92a
Iamneo <techadmin@iamneo.ai>
2023-09-21 10:21:27 +0000

    Backup Commit

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -10,14 +10,14 @@ provider "aws" {
     access_key = "AKIAY6OXM4NMALTE27NW"
     secret_key = "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"
 }
-resource "aws_instance" "linux" {
-    ami          = "ami-"Oe9aeO9818b5c6f7O"
+resource "aws_instance" "linuxserver" {
+    ami          = "ami-0dacd9d37849a9f38"
     instance_type = "t2.micro"
 
     tags = {
-        Name = "MyEC2Instance"
+        Name = "MyLinuxServer"
     }
 }
 output "public_ip" {
-    value = aws_instance.example.public_ip
+    value = aws_instance.linux_server.public_ip
 }
