69a53e0e2d6361231548b0650e65f919e4c1fedb
Iamneo <techadmin@iamneo.ai>
2023-09-21 09:51:43 +0000

    Backup Commit

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "your_access_key_here"
-    secret_key = "your_secret_key_here"
+    access_key = "AKIAY6OXM4NMALTE27NW"
+    secret_key = "5eGUQxiqXM9Zt/MMSBji5A13hUrbGoomy0ZCINIr"
     region     = "ap-southeast-1" # Singapore region
 }
 resource "aws_instance" "example" {
